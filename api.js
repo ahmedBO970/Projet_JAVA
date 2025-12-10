4f00120b33b283ba560dddd361a5345da0a059ee
ahmedBO970 <bouzidia970@gmail.com>
2025-12-10 15:55:01 +0100

    Création de api.js

---
+++ api.js
@@ -0,0 +1,21 @@
+const KEY_API = "15bde907";
+const URL_API = "https://www.omdbapi.com/";
+
+export async function get_filme(requete) {
+  try {
+    const url = `${URL_API}?apikey=${KEY_API}&${requete}`;
+
+    const reponse = await fetch(url);
+    const data = await reponse.json();
+
+    if (data.Response === "False") {
+      console.warn("Erreur :", data.Error);
+      return null;
+    }
+
+    return data;
+  } catch (error) {
+    console.error("Erreur lors de l'appel API :", error);
+    return null;
+  }
+}
